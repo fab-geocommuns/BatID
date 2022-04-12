8626f0caf0b71cce1451042e4e8ae1a4a95c480f
fe51 <veith.felix@gmail.com>
2022-04-12 09:29:25 +0200

    feat: added parcelle table

--- src/db/core_data_structure.sql
+++ src/db/core_data_structure.sql
@@ -40,3 +40,20 @@ COMMENT ON COLUMN tup.code_tup IS 'Identifiant de tup';
 COMMENT ON COLUMN tup.geomtup IS 'Géometrie de la TUP.';
 
 
+
+-- Parcelle
+--------------------
+
+CREATE TABLE parcelle(
+	id_parcelle text PRIMARY KEY,
+	code_parcelle text,
+	id_tup text REFERENCES tup (id_tup)
+);
+
+COMMENT ON TABLE parcelle IS 'Parcelles au sens du cadastre';
+
+COMMENT ON COLUMN parcelle.id_parcelle IS 'clef primaire';
+COMMENT ON COLUMN parcelle.code_parcelle IS 'Identifiant de parcelle';
+COMMENT ON COLUMN parcelle.id_tup IS 'clef étrangère de la table des TUPs';
+
+
