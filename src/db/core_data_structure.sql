4eeec06f2227d5e40c65e72c4d8d7ef4ddce22ec
fe51 <veith.felix@gmail.com>
2022-04-12 09:28:50 +0200

    feat: added TUP table

--- src/db/core_data_structure.sql
+++ src/db/core_data_structure.sql
@@ -14,3 +14,29 @@ CREATE EXTENSION postgis;
 
 -- set default schema
 SET SEARCH_PATH TO rnb,public;
+
+
+--------------------
+--- Tables creation
+--------------------
+
+
+
+-- TUP: tables des parcelles unifiées
+--------------------
+
+--table
+CREATE TABLE tup(
+	id_tup text PRIMARY KEY,
+	code_tup text,
+	geomtup geometry(multipolygon, 2154)
+);
+
+--table documentation
+COMMENT ON TABLE tup IS 'Tables des parcelles unifiées (TUP) issues du modèle CEREMA des Fichiers Fonciers';
+
+COMMENT ON COLUMN tup.id_tup IS 'Clef primaire.';
+COMMENT ON COLUMN tup.code_tup IS 'Identifiant de tup';
+COMMENT ON COLUMN tup.geomtup IS 'Géometrie de la TUP.';
+
+
