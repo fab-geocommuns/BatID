e4098394098087266f52e38d156adf193bc73558
fe51 <veith.felix@gmail.com>
2022-04-12 10:11:56 +0200

    feat: added xlsx doc script to utils

---
+++ src/db/utils/create_docs.py
@@ -0,0 +1,31 @@
+from sqlalchemy import create_engine
+from sqlalchemy import inspect
+from geoalchemy2 import Geometry #to be able to handle geometry type
+import pandas as pd
+
+import src.db.config as cfg
+
+engine = create_engine(cfg.DATABASE_CONNECTION_URI)
+
+inspector = inspect(engine)
+
+writer = pd.ExcelWriter('data_dict_batiment.xlsx')
+
+tables = pd.DataFrame(columns=['table', 'description'], dtype=object)
+
+default_schema = "rnb"
+
+for t in inspector.get_table_names(default_schema):
+    tables = tables.append({"table": t,
+                            "description":
+                            inspector.get_table_comment(t,
+                                                        schema=default_schema)["text"]},
+                            ignore_index=True)
+
+    df = pd.DataFrame(inspector.get_columns(table_name=t,
+                                            schema=default_schema))
+    df = df.drop(columns=['default', "autoincrement"])
+    df.to_excel(writer, sheet_name=t, index=False)
+
+tables.to_excel(writer, sheet_name='TABLES DESCRIPTION', index=False)
+writer.save()
